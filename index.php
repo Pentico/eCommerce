9962862e17f4dbd4b7fb1b312dae0d3d72bf744e
Alfred Morulane <morulane.ta@hotmail.com>
2016-04-25 21:22:46 +0200

    edited the index.php file , included the required backbone file . TODO : The footer file and the header file ....

--- index.php
+++ index.php
@@ -5,4 +5,14 @@
  * Date: 2016/04/16
  * Time: 8:48 PM
  */
-echo "well";
+
+require('./includes/Config.php');
+require (MYSQL); //The database connection
+include ('./includes/header.html');
+
+?> <h3> Welcome</h3>
+   <p class="lead"> e-commerce for all !</p>
+
+<?php
+include ('./includes/footer.html'); //TODO : add the footer file to the project
+?>
